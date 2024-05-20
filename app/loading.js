2085761088ff2f9ca01cb53c6bfe598869df9292
Taofeek Adeniyi <105851811+tee4tao@users.noreply.github.com>
2024-05-20 12:22:28 +0100

    corrections

--- app/loading.js
+++ app/loading.js
@@ -1,12 +1,11 @@
 const loading = () => {
     return (
-      <section className="flex justify-center items-center w-screen h-screen fixed top-0 right-0">
-        <div className="loading"></div>
-        {/* <span className="loading loading-ball loading-xs"></span>
+      <div className="flex justify-center items-center w-screen h-screen fixed top-0 right-0">
+        <span className="loading loading-ball loading-xs"></span>
         <span className="loading loading-ball loading-sm"></span>
         <span className="loading loading-ball loading-md"></span>
-        <span className="loading loading-ball loading-lg"></span> */}
-      </section>
+        <span className="loading loading-ball loading-lg"></span>
+      </div>
     );
   };
   
