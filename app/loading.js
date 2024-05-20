66be15b671acc97c8772493c5e656af40223e434
Taofeek Adeniyi <105851811+tee4tao@users.noreply.github.com>
2024-05-20 08:25:39 +0100

    changed the loading animation

--- app/loading.js
+++ app/loading.js
@@ -1,10 +1,18 @@
 const loading = () => {
     return (
       <div className="flex justify-center items-center w-screen h-screen ">
-        <span className="loading loading-ball loading-xs"></span>
+        <span className="loading loading-spinner text-primary"></span>
+<span className="loading loading-spinner text-secondary"></span>
+<span className="loading loading-spinner text-accent"></span>
+<span className="loading loading-spinner text-neutral"></span>
+<span className="loading loading-spinner text-info"></span>
+<span className="loading loading-spinner text-success"></span>
+<span className="loading loading-spinner text-warning"></span>
+<span className="loading loading-spinner text-error"></span>
+        {/* <span className="loading loading-ball loading-xs"></span>
         <span className="loading loading-ball loading-sm"></span>
         <span className="loading loading-ball loading-md"></span>
-        <span className="loading loading-ball loading-lg"></span>
+        <span className="loading loading-ball loading-lg"></span> */}
       </div>
     );
   };
