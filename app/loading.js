fc41a22801de22a36b1a6a5f9f2bae5e4bd858a8
Taofeek Adeniyi <105851811+tee4tao@users.noreply.github.com>
2024-05-20 11:08:37 +0100

    worked on loading animation

--- app/loading.js
+++ app/loading.js
@@ -1,15 +1,12 @@
 const loading = () => {
     return (
-      <div className="flex justify-center items-center w-screen h-screen ">
-     <span className="loading loading-bars loading-xs"></span>
-<span className="loading loading-bars loading-sm"></span>
-<span className="loading loading-bars loading-md"></span>
-<span className="loading loading-bars loading-lg"></span>
+      <section className="flex justify-center items-center w-screen h-screen fixed top-0 right-0">
+        <div className="loading"></div>
         {/* <span className="loading loading-ball loading-xs"></span>
         <span className="loading loading-ball loading-sm"></span>
         <span className="loading loading-ball loading-md"></span>
         <span className="loading loading-ball loading-lg"></span> */}
-      </div>
+      </section>
     );
   };
   
